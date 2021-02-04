dd1e2d7955ce2c8959712e0b2ab31b44578c5c66
Taylor Thornton <thornrae@gmail.com>
2021-02-03 19:45:50 -0800

    cocktailHandler function working

--- server.js
+++ server.js
@@ -31,9 +31,46 @@ const client = new pg.Client(process.env.DATABASE_URL);// TAKE IN PATH OF DATABA
 // app.get('/new', searchPage);
 // app.get('/', favoritePage);
 
-app.use('*', (req, res) => {
-  res.status(404).send('Something is wrong');
-});
+// app.use('*', (req, res) => {
+//   res.status(404).send('Something is wrong');
+// });
+
+// ---------------- COCKTAILS API ------------------------
+app.get('/', cocktailHandler);
+// app.get('showDrinks', drinkDetails);
+
+function cocktailHandler(req, res) {
+  // let drinkType = request.query.drinkType;
+  // let url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${drinkType}`;
+  let url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka';
+
+  superagent.get(url)
+    .then( value => {
+      let drinkSearch = value.body.drinks;
+      let cocktailIds = [];
+      drinkSearch.forEach( drink =>{
+        let newdrink = new CocktailGenerator(drink);
+        cocktailIds.push(newdrink);
+      });
+      res.status(200).send(cocktailIds);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+// function drinkDetails(req, res) {
+//   let url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
+  
+// }
+
+function CocktailGenerator(drink) {
+  this.idDrink = drink.idDrink;
+  this.drinkName = drink.strDrink;
+  this.img = drink.strDrinkThumb;
+}
+
+
 
 client.connect()
   .then(() => {
@@ -41,5 +78,6 @@ client.connect()
       console.log(`now listening on port ${PORT}`);
     });
   }).catch(error => console.error(error));
+
 
 
